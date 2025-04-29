a4db9bb434d02393847658c44bb932f9dc330c5e
IsuruAchintha <89300672+AchinthaDavidson@users.noreply.github.com>
2025-04-29 11:26:43 +0530

    0.0.6

--- redux/slices/brandApiSlice.ts
+++ redux/slices/brandApiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const brandApiSlice = createApi({
   reducerPath: 'brandApi',
 
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/'  }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://surangacellcare-dev.netlify.app/api/'  }),
 
   tagTypes: ['Brand'],
   endpoints: (builder) => ({
